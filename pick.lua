7a5ce721a0b19d28e132d4fd6a7974834a8a1559
Nathan Salapat <nathan@nathansalapat.com>
2021-07-12 20:01:47 -0500

    fix normal drops.

--- pick.lua
+++ pick.lua
@@ -31,10 +31,12 @@ local old_handle_node_drops = minetest.handle_node_drops
 
 function minetest.handle_node_drops(pos, drops, digger)
    if not digger or digger:get_wielded_item():get_name() ~= 'wow:pick' then
+      print ('not using wow pick')
       return old_handle_node_drops(pos, drops, digger)
    end
    local wow_chance = math.random(1,20) --Increase the second number to decrease the chance.
    if wow_chance == 6 then
       minetest.add_item(pos, 'wow:coin')
    end
+   return old_handle_node_drops(pos, drops, digger)
 end
