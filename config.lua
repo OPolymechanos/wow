4940a8ffcba6eff836371dcafa6719a9962dfd7e
Polymechanos <daniel@mbbm.blue>
2021-10-28 17:12:42 -0400

    added config file

---
+++ config.lua
@@ -0,0 +1,9 @@
+-- WOW Configuration
+-- Additional settings / features that may be toggled on/off
+
+-- FRAGMENTS -- 
+-- Turn wow coin fragments on/off. If wow_fragments is set to 'true', mese pickaxes will have a small chance of dropping pieces of wow coins. These pieces may then be combined to form complete coins. This feature is provided mainly to allow for wow coins to be produced in 'survival' mode if this mod be added to pre-existing games or servers (i.e. cases in which new map generation isn't possible).
+-- Default is 'false'
+WOW_FRAGMENTS = false 
+
+
